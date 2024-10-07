e055445ffa71bdaf5294ca1d2799bb53dafb2c7b
Jay Mingle <jkawudi@gmail.com>
2024-10-07 17:59:13 +0000

    Commit

--- src/stories/Button/Button.stories.jsx
+++ src/stories/Button/Button.stories.jsx
@@ -7,5 +7,5 @@ export default {
 }
 
 export const Default = () => <Button/>
-export const Colors = () => options.colors((color, index) => <Button key={index} color={color}/>)
-export const Sizes = () => options.size((size, index) => <Button key={index} size={size}/>)
+export const Colors = () => options.colors.map((color, index) => <Button key={index} color={color}/>)
+export const Sizes = () => options.sizes.map((size, index) => <Button key={index} size={size}/>)

1debe929ca1409b6673778bf6667b3fade20e0bf
Jay Mingle <jkawudi@gmail.com>
2024-10-07 18:46:13 +0000

    Commit

--- src/stories/AdminButton/AdminButton.stories.jsx
+++ src/stories/AdminButton/AdminButton.stories.jsx
@@ -3,7 +3,9 @@ import {options} from "./constants.js"
 
 export default {
     title: "Admin Button",
-    component: Button
+    component: AdminButton
 }
 
 export const Default = () => <AdminButton/>
+export const Colors = () => options.colors.map((color, index) => <AdminButton key={index} color={color}/>)
+export const Sizes = () => options.sizes.map((size, index) => <AdminButton key={index} size={size}/>)

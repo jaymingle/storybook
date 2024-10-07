45bc3624047a9aba8abf0f03c9c50b996276bf65
Jay Mingle <jkawudi@gmail.com>
2024-10-07 18:54:42 +0000

    Commit

---
+++ src/stories/CustomerButton/CustomerButton.stories.jsx
@@ -0,0 +1,10 @@
+import CustomerButton from "./CustomerButton.jsx";
+import {options} from "./constants.js";
+
+
+export default {
+    title: 'Customer Button',
+    component: CustomerButton
+}
+
+export const Default = () => <CustomerButton/>

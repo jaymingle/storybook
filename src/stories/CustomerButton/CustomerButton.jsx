8d60839b4dce1179db47ee0048e99864aecc7d20
Jay Mingle <jkawudi@gmail.com>
2024-10-07 18:52:45 +0000

    Commit

--- src/stories/CustomerButton/CustomerButton.jsx
+++ src/stories/CustomerButton/CustomerButton.jsx
@@ -1,8 +1,14 @@
 import {options} from "./constants.js";
-import button from "../Button/Button.jsx";
+import PropTypes from "prop-types";
 
 const CustomerButton = ({children='Customer Button', colors ='tertiary', sizes='lg'}) => {
     return <button>{children}</button>
+}
+
+CustomerButton.propTypes = {
+    children: PropTypes.node.isRequired,
+    colors: PropTypes.oneOf(options.colors),
+    sizes: PropTypes.oneOf(options.sizes)
 }
 
 export default CustomerButton;

abec563c975075dfd6aaa4e0d128f603a9c96992
Jay Mingle <jkawudi@gmail.com>
2024-10-08 20:49:11 +0000

    Commit

---
+++ src/stories/UserButton/UserButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {options} from "./constants.js";
+import PropTypes from "prop-types";
+
+const UserButton = ({children='User Button', colors}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default UserButton;
---
+++ src/stories/UserButton/constants.js
@@ -0,0 +1,4 @@
+export const options = {
+    size: ['sm', 'md', 'lg'],
+    color: ['primary', 'secondary', 'tertiary']
+}

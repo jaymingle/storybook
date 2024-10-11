573f5b93263995a791bc90518203dd265ceec16f
Jay Mingle <jkawudi@gmail.com>
2024-10-11 02:37:34 +0000

    Commit

--- src/stories/Card/Card.jsx
+++ src/stories/Card/Card.jsx
@@ -5,11 +5,19 @@ import {options} from "./constants.js";
 import './Card.css'
 import classNames from "classnames";
 
-const Card = ({children = "I am a Card", color = "primary", size = "md"}) => {
+const Card = ({
+                  children = "I am a Card",
+                  color = "primary",
+                  size = "md",
+                isClickable,
+                isDraggable
+}) => {
     return (
         <div className={classNames('card', {
             [`color-${color}`]: color,
             [`size-${color}`]: size,
+            "is-clickable": isClickable,
+            "is-draggable": isDraggable
 
         })}>
             {children}

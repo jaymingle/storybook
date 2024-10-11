c036fc19aac8eb6eed45859582b70e5ae8bfc3e6
Jay Mingle <jkawudi@gmail.com>
2024-10-11 02:31:00 +0000

    Commit

--- src/stories/Card/Card.stories.jsx
+++ src/stories/Card/Card.stories.jsx
@@ -6,6 +6,11 @@ export default {
     component: Card,
 }
 
-export const Default = () => <Card />
+const Template = args => <Card {...args}/>
+export const Default = Template.bind({})
+export const Clickable = Template.bind({})
+export const Draggable = Template.bind({})
+
+
 export const Colors = () => options.colors.map((color, index) => <Card key={index} color={color}/>)
 export const Sizes = () => options.sizes.map((size, index) => <Card key={index} size={size}/>)
